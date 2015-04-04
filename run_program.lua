adf808b63ec88d577858354b77e40f89a1e6d08e
davidp <davidp@huelvayork.com>
2015-04-04 02:00:39 +0200

    setup STA using config.lc variables

--- run_program.lua
+++ run_program.lua
@@ -1,2 +1,8 @@
 -- Start your normal program routines here 
 print("Execute code")
+dofile("config.lc")
+wifi.setmode(wifi.STATION)
+wifi.sta.config(ssid,password)
+wifi.sta.connect()
+
+dofile("program.lc")
